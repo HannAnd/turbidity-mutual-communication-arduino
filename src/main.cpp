f178099841639f2ee39b14bc3e10c138124bbcae
Hannah Anderson <anderh3@mcmcaster.ca>
2021-05-10 14:49:08 -0400

    Replaced else-if statements with switch case contained in new function.

--- src/main.cpp
+++ src/main.cpp
@@ -5,19 +5,27 @@
 // needed when dealing with Arduino code outside of the Arduino IDE:
 # include <Arduino.h>
 
-// specifying the analog pin for the turbidity sensor:
+// specifying the analog pins for the turbidity sensors:
 # define TURBID1 A1
+# define TURBID2 A2
+# define TURBID3 A3
+
 // specifying the number of samples to be averaged to smooth (average)
 // sensor readings:
 # define NUMSAMPLES 5
 
 // handing the sample number to the sample variables for smoothing:
-int samples_t1[NUMSAMPLES];
+int samples_t[NUMSAMPLES];
+
 // defining the pins for the different relay channels:
 int relay1 = 2;
 int relay2 = 3;
 int relay3 = 4;
 int relay4 = 5;
+int relay5 = 6;
+int relay6 = 7;
+int relay7 = 8;
+int relay8 = 9;
 
 void setup() {
   // opens a port that samples at the standard 9600 bits per second:
@@ -38,82 +46,164 @@ void setup() {
   digitalWrite(relay4, HIGH);
 }
 
-void loop() {
-  // creates an unsinged integer for use as a counter:
+
+// function for setting channel states:
+void serialparse(int command) {
+  // creates an unsigned integer for use as a counter:
   uint8_t i;
   // for use in smoothing noisy sensor readings:
-  float ave_turb1;
+  float ave_turb;
+  float voltage_t;
 
+  switch (command) {
+
+    // read turbidity sensor 1:
+    case 1:
+    samples_t[NUMSAMPLES] = 0;
+    analogRead(TURBID1);  // read to align multiplexer to A1:
+    delay(10);
+    analogRead(TURBID1);  // unused reading to deal with ADC problems:
+    delay(50);
+
+    for (i=0; i< NUMSAMPLES; i++)  {
+      samples_t[i] = analogRead(TURBID1);
+      delay(50);
+    }
+
+    // resetting the average variable between iterations:
+    ave_turb = 0;
+    // averaging the above samples:
+    for (i=0; i< NUMSAMPLES; i++)  {
+      ave_turb += samples_t[i];
+    }
+    ave_turb /= NUMSAMPLES;
+
+    // converting analog readings (which go from 0-1023) to voltages (0-5v):
+    voltage_t = ave_turb*(5.0/1023.0);  //might leave analog readings as-is and learn to just convert NTU from that
+
+    // sending voltage readings through Serial to the Pi:
+    Serial.println(voltage_t);
+    break;
+
+    // read turbidity sensor 2:
+    case 2:
+    samples_t[NUMSAMPLES] = 0;
+    analogRead(TURBID2);  // read to align multiplexer to A2:
+    delay(10);
+    analogRead(TURBID2);  // unused reading to deal with ADC problems:
+    delay(50);
+
+    for (i=0; i< NUMSAMPLES; i++)  {
+      samples_t[i] = analogRead(TURBID1);
+      delay(50);
+    }
+
+    // resetting the average variable between iterations:
+    ave_turb = 0;
+    // averaging the above samples:
+    for (i=0; i< NUMSAMPLES; i++)  {
+      ave_turb += samples_t[i];
+    }
+    ave_turb /= NUMSAMPLES;
+
+    // converting analog readings (which go from 0-1023) to voltages (0-5v):
+    voltage_t = ave_turb*(5.0/1023.0);  //might leave analog readings as-is and learn to just convert NTU from that
+
+    // sending voltage readings through Serial to the Pi:
+    Serial.println(voltage_t);
+    break;
+
+    // read turbidity sensor 3:
+    case 3:
+    samples_t[NUMSAMPLES] = 0;
+    analogRead(TURBID3);  // read to align multiplexer to A3:
+    delay(10);
+    analogRead(TURBID3);  // unused reading to deal with ADC problems:
+    delay(50);
+
+    for (i=0; i< NUMSAMPLES; i++)  {
+      samples_t[i] = analogRead(TURBID1);
+      delay(50);
+    }
+
+    // resetting the average variable between iterations:
+    ave_turb = 0;
+    // averaging the above samples:
+    for (i=0; i< NUMSAMPLES; i++)  {
+      ave_turb += samples_t[i];
+    }
+    ave_turb /= NUMSAMPLES;
+
+    // converting analog readings (which go from 0-1023) to voltages (0-5v):
+    voltage_t = ave_turb*(5.0/1023.0);  //might leave analog readings as-is and learn to just convert NTU from that
+
+    // sending voltage readings through Serial to the Pi:
+    Serial.println(voltage_t);
+    break;
+
+    // all cases below are for opening the appropriate channel at the
+    // command of the Pi:
+
+    // opening channel 1 (should occur for plain water):
+    case 4:
+    digitalWrite(relay1, LOW);
+    digitalWrite(relay2, HIGH);
+    digitalWrite(relay3, HIGH);
+    digitalWrite(relay4, HIGH);
+    break;
+
+    // opening channel 2 (should occur for the weak coffee):
+    case 5:
+    digitalWrite(relay1, HIGH);
+    digitalWrite(relay2, LOW);
+    digitalWrite(relay3, HIGH);
+    digitalWrite(relay4, HIGH);
+    break;
+
+    // opening channel 3 (should occur for the strong coffee):
+    case 6:
+    digitalWrite(relay1, HIGH);
+    digitalWrite(relay2, HIGH);
+    digitalWrite(relay3, LOW);
+    digitalWrite(relay4, HIGH);
+    break;
+
+    // opening channel 4 (should occur if the sensor is suspended in air):
+    case 7:
+    digitalWrite(relay1, HIGH);
+    digitalWrite(relay2, HIGH);
+    digitalWrite(relay3, HIGH);
+    digitalWrite(relay4, LOW);
+    break;
+
+    // all channels off:
+    // should happen when the Python code starts running or
+    // when there is an invalid turbidity reading:
+    case 8:
+    digitalWrite(relay1, HIGH);
+    digitalWrite(relay2, HIGH);
+    digitalWrite(relay3, HIGH);
+    digitalWrite(relay4, HIGH);
+    break;
+
+    default:
+    // no action:
+    break;
+  }
+}
+
+void loop() {
   // checking if there's anything in Serial that's been sent by the Pi:
   // for this setup the Arduino will only act if told to by the Pi:
   if (Serial.available() > 0) {
     // saving the command the Pi sends
-    String command = Serial.readStringUntil('\n');
+    String pi_out = Serial.readStringUntil('\n');
     // removing any excess whitespace the Pi might have sent with the command
-    command.trim();
+    pi_out.trim();
+    // converting the Serial string to integer for the switch case:
+    int command = pi_out.toInt();
 
-    // collecting five turbidity sensor readings (with a delay) to average
-    // to smooth noisy sensor readings
-    if (command.equals("turbidread")) {
-      samples_t1[NUMSAMPLES] = 0;
-      for (i=0; i< NUMSAMPLES; i++) {
-        samples_t1[i] = analogRead(TURBID1);
-        delay(50);
-      }
+    serialparse(command);
 
-      // reseting the average variable between iterations:
-      ave_turb1 = 0;
-      // averaging the above samples:
-      for (i=0; i< NUMSAMPLES; i++) {
-        ave_turb1 += samples_t1[i];
-      }
-      ave_turb1 /= NUMSAMPLES;
-
-      // converting the analog readings (which go from 0-1023) to
-      // voltages (0-5v):
-      float voltage_t1 = ave_turb1*(5.0/1023.0);
-
-      // sending voltage readings to Serial and the Pi:
-      Serial.println(voltage_t1);
-    }
-
-    // opening the appropriate relay channel when the Pi says so:
-    // opening channel 1 (should occur for plain water):
-    if (command.equals("channel1")) {
-      digitalWrite(relay1, LOW);
-      digitalWrite(relay2, HIGH);
-      digitalWrite(relay3, HIGH);
-      digitalWrite(relay4, HIGH);
-    }
-    // opening channel 2 (should occur for the weak coffee):
-    else if (command.equals("channel2")) {
-      digitalWrite(relay1, HIGH);
-      digitalWrite(relay2, LOW);
-      digitalWrite(relay3, HIGH);
-      digitalWrite(relay4, HIGH);
-    }
-    // opening channel 3 (should occur for the strong coffee):
-    else if (command.equals("channel3")) {
-      digitalWrite(relay1, HIGH);
-      digitalWrite(relay2, HIGH);
-      digitalWrite(relay3, LOW);
-      digitalWrite(relay4, HIGH);
-    }
-    // opening channel 4 (should occur if the sensor is suspended in air):
-    else if (command.equals("channel4")) {
-      digitalWrite(relay1, HIGH);
-      digitalWrite(relay2, HIGH);
-      digitalWrite(relay3, HIGH);
-      digitalWrite(relay4, LOW);
-    }
-    // all channels off:
-    // should happen when the Python code starts running or
-    // when there is an invalid turbidity reading:
-    else if (command.equals("off")) {
-      digitalWrite(relay1, HIGH);
-      digitalWrite(relay2, HIGH);
-      digitalWrite(relay3, HIGH);
-      digitalWrite(relay4, HIGH);
-    }
   }
 }
